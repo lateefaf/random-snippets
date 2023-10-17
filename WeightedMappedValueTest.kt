4ec62262abca5c690b0a4e51ad3142a96e2996ee
Afnan <alateef4@gmail.com>
2023-10-17 09:39:27 -0400

    more commits

--- WeightedMappedValueTest.kt
+++ WeightedMappedValueTest.kt
@@ -1,29 +1,90 @@
+import org.junit.jupiter.api.Test
 import java.io.File
-import java.io.FileWriter
+import kotlin.random.Random
+import kotlin.test.assertTrue
 
-fun main() {
-    // Initialize the WeightedMappedValueStrategy object with a test file path
-    val strategy = WeightedMappedValueStrategy("weights.txt")
+class WeightedMappedValueStrategyTest {
 
-    // Initialize a GraphState object and set targetEntity and targetColumn for demonstration
-    val graphState = GraphState()
-    strategy.targetEntity = "TestEntity"
-    strategy.targetColumn = "TestColumn"
+    @Test
+    fun testProduce() {
+        // Test with single key, unequal weights (A: B:1, C:3)
+        runTest(
+                fileContent = """
+                A,B,1
+                A,C,3
+            """.trimIndent(),
+                lookupValue = "A",
+                expectedProbabilities = mapOf("B" to 0.25, "C" to 0.75),
+                tolerance = 0.05
+        )
 
-    // A map to store the frequency of each generated value
-    val frequencyMap: MutableMap<String, Int> = mutableMapOf()
+        // Test with single key, equal weights (B: D:1, E:1)
+        runTest(
+                fileContent = """
+                B,D,1
+                B,E,1
+            """.trimIndent(),
+                lookupValue = "B",
+                expectedProbabilities = mapOf("D" to 0.5, "E" to 0.5),
+                tolerance = 0.05
+        )
 
-    // Generate values 10000 times
-    for (i in 1..10000) {
-        // For demonstration, we set the targetColumn to 'A' and 'B' alternately to test the probabilities
-        graphState[strategy.targetEntity, strategy.targetColumn] = if (i % 2 == 0) "A" else "B"
+        // Test with single key, various weights (C: F:1, G:2, H:2)
+        runTest(
+                fileContent = """
+                C,F,1
+                C,G,2
+                C,H,2
+            """.trimIndent(),
+                lookupValue = "C",
+                expectedProbabilities = mapOf("F" to 0.2, "G" to 0.4, "H" to 0.4),
+                tolerance = 0.05
+        )
 
-        // Generate the new value based on the probabilities
-        strategy.produce(graphState)
-
-        // Record the frequency of each generated value
-        val generatedValue = graphState[strategy.targetEntity, strategy.targetColumn].toString()
-        frequencyMap[generatedValue] = frequencyMap.getOrDefault(generatedValue, 0) + 1
+        // Test with incorrect weights (should throw an exception)
+        try {
+            runTest(
+                    fileContent = """
+                    A,D,0
+                    A,E,-1
+                """.trimIndent(),
+                    lookupValue = "A",
+                    expectedProbabilities = mapOf(),
+                    tolerance = 0.05
+            )
+            assertTrue(false, "Should have thrown an IllegalArgumentException")
+        } catch (e: IllegalArgumentException) {
+            // Test passed
+        }
     }
 
-g=
+    private fun runTest(fileContent: String, lookupValue: String, expectedProbabilities: Map<String, Double>, tolerance: Double) {
+        val testFilePath = "testFile.csv"
+        File(testFilePath).writeText(fileContent)
+
+        val strategy = WeightedMappedValueStrategy()
+        val mockGraphState = GraphState()
+
+        strategy.getArgumentDefinitions()[WeightedMappedValueStrategy.ARG_FILE_PATH]?.load(testFilePath)
+
+        val iterations = 10000
+        val frequencyMap: MutableMap<String, Int> = mutableMapOf()
+
+        for (i in 1..iterations) {
+            mockGraphState[strategy.targetEntity, strategy.targetColumn] = lookupValue
+            strategy.random = Random.Default
+            strategy.produce(mockGraphState)
+            val value = mockGraphState[strategy.targetEntity, strategy.targetColumn]?.toString()
+            frequencyMap[value.toString()] = frequencyMap.getOrDefault(value.toString(), 0) + 1
+        }
+
+        for ((value, expectedProb) in expectedProbabilities) {
+            val count = frequencyMap[value] ?: 0
+            val actualProb = count.toDouble() / iterations
+            assertTrue(
+                    actualProb >= expectedProb - tolerance && actualProb <= expectedProb + tolerance,
+                    "$value was selected with a frequency of $actualProb but expected around $expectedProb"
+            )
+        }
+    }
+}
