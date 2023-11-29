846b1a888398ec68be8bb2d8c04b98af9654e2e5
lateefaf <45246559+lateefaf@users.noreply.github.com>
2023-11-29 09:30:21 -0500

    more commits

--- SQLSerializer.kt
+++ SQLSerializer.kt
@@ -128,3 +128,105 @@ class SQLSerializer(
 // Usage Example
 val columnTypes = mapOf("columnName1" to "VARCHAR", "columnName2" to "INT")
 val serializer = SQLSerializer("tableName", columnTypes)
+
+
+
+val columnTypes = mapOf(
+        "columnName1" to SQLStringType(255),
+        "columnName2" to SQLFloatType(10, 2),
+        "columnName3" to SQLVarBinaryType(64)
+)
+val serializer = SQLSerializer("tableName", columnTypes)
+
+
+private fun formatColumn(columnName: String, value: Any): String {
+    val dataType = columnDataTypes[columnName] ?: return value.toString()
+
+    return when (dataType) {
+        is SQLStringType -> formatString(value.toString(), dataType.length)
+        is SQLIntegerType -> value.toString()
+        is SQLDecimalType -> formatDecimal(value as BigDecimal, dataType.precision, dataType.scale)
+        is SQLFloatType -> formatFloat(value as Float, dataType.precision, dataType.scale)
+        is SQLBinaryType -> formatBinary(value as ByteArray, dataType.length)
+        is SQLBlobType -> formatBlob(value as ByteArray)
+        is SQLDateType -> formatDate(value as Date)
+        is SQLTimeType -> formatTime(value as Date, dataType.fractionalSecondsPrecision)
+        is SQLDateTimeType -> formatDateTime(value as Date, dataType.fractionalSecondsPrecision)
+        is SQLTimestampType -> formatTimestamp(value as Date, dataType.fractionalSecondsPrecision)
+        is SQLYearType -> formatYear(value as Date)
+        is SQLTextType -> formatText(value.toString())
+        is SQLEnumType, is SQLSetType -> formatEnumSet(value.toString())
+        // ... additional handling for other types if necessary
+        else -> value.toString()
+    }
+}
+
+private fun formatString(value: String, length: Int): String {
+    val truncatedValue = if (length >= 0) value.substring(0, minOf(value.length, length)) else value
+    return "'${truncatedValue.replace("'", "''")}'"
+}
+
+private fun formatDecimal(value: BigDecimal, precision: Int, scale: Int): String {
+    return value.setScale(scale, RoundingMode.HALF_UP).toPlainString()
+}
+
+private fun formatFloat(value: Float, precision: Int, scale: Int): String {
+    return BigDecimal.valueOf(value.toDouble()).setScale(scale, RoundingMode.HALF_UP).toPlainString()
+}
+
+private fun formatBinary(value: ByteArray, length: Int): String {
+    val formattedValue = value.take(length).joinToString("") { "%02x".format(it) }
+    return "0x$formattedValue"
+}
+
+private fun formatBlob(value: ByteArray): String {
+    val formattedValue = value.joinToString("") { "%02x".format(it) }
+    return "0x$formattedValue"
+}
+
+private fun formatDate(value: Date): String {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+    return "'${dateFormat.format(value)}'"
+}
+
+private fun formatTime(value: Date, fsp: Int): String {
+    val timeFormat = SimpleDateFormat("HH:mm:ss.${"S".repeat(fsp)}")
+    return "'${timeFormat.format(value)}'"
+}
+
+private fun formatDateTime(value: Date, fsp: Int): String {
+    val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.${"S".repeat(fsp)}")
+    return "'${dateTimeFormat.format(value)}'"
+}
+
+private fun formatTimestamp(value: Date, fsp: Int): String {
+    val timestampFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.${"S".repeat(fsp)}")
+    return "'${timestampFormat.format(value)}'"
+}
+
+private fun formatYear(value: Date): String {
+    val yearFormat = SimpleDateFormat("yyyy")
+    return "'${yearFormat.format(value)}'"
+}
+
+private fun formatText(value: String): String {
+    return "'${value.replace("'", "''")}'"
+}
+
+private fun formatEnumSet(value: String): String {
+    return "'${value.replace("'", "''")}'"
+}
+
+sealed class SQLDataType
+
+data class SQLStringType(val length: Int) : SQLDataType()
+data class SQLIntegerType : SQLDataType() // For INT, TINYINT, SMALLINT, MEDIUMINT, BIGINT
+data class SQLDecimalType(val precision: Int, val scale: Int) : SQLDataType() // For DECIMAL, NUMERIC
+data class SQLFloatType(val precision: Int, val scale: Int) : SQLDataType() // For FLOAT, DOUBLE
+data class SQLBinaryType(val length: Int) : SQLDataType() // For BINARY, VARBINARY
+data class SQLBlobType : SQLDataType() // For TINYBLOB, BLOB, MEDIUMBLOB, LONGBLOB
+data class SQLDateType : SQLDataType() // For DATE
+data class SQLTimeType(val fractionalSecondsPrecision: Int) : SQLDataType() // For TIME(fsp)
+data class SQLDateTimeType(val fractionalSecondsPrecision: Int) : SQLDataType() // For DATETIME(fsp)
+data class SQLTimestampType(val fractionalSecondsPrecision: Int) : SQLDataType() // For TIMESTAMP(fsp)
+data class SQLYearType : SQLDataType() // For YEAR
