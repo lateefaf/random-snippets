b6b75333221c9775488bd6ff31adbd1339efbe03
Lateef <alateef4@gmail.com>
2023-12-07 10:01:03 -0500

    unit tests

--- SQLSerializerUnitTests.kt
+++ SQLSerializerUnitTests.kt
@@ -135,4 +135,103 @@ class SQLSerializerUnitTests {
         val floatType = SQLFloatType(2, 3)
         assertThrows<IllegalArgumentException> { floatType.validate() }
     }
+
+    //SQLBinaryType
+    // Test for the format function
+
+    @Test
+    fun `if a ByteArray is formatted with SQLBinaryType, then it should be converted to a hexadecimal string of the specified length`() {
+        val binaryType = SQLBinaryType(4)
+        val byteArray = byteArrayOf(0x01, 0x02, 0x03, 0x04, 0x05)
+        val result = binaryType.format(byteArray)
+        assertEquals("0x01020304", result)  // 4-byte length limit
+    }
+
+    @Test
+    fun `if a ByteArray shorter than the specified length is formatted, then the entire array should be converted without padding`() {
+        val binaryType = SQLBinaryType(6)
+        val byteArray = byteArrayOf(0x01, 0x02, 0x03)
+        val result = binaryType.format(byteArray)
+        assertEquals("0x010203", result)  // No padding added
+    }
+
+    // Test for the validate function
+
+    @Test
+    fun `if length is positive for SQLBinaryType, then validate should pass`() {
+        val binaryType = SQLBinaryType(5)
+        assertDoesNotThrow { binaryType.validate() }
+    }
+
+    @Test
+    fun `if length is negative for SQLBinaryType, then validate should fail`() {
+        val binaryType = SQLBinaryType(-1)
+        assertThrows<IllegalArgumentException> { binaryType.validate() }
+    }
+
+    @Test
+    fun `if length is zero for SQLBinaryType, then validate should pass`() {
+        val binaryType = SQLBinaryType(0)
+        assertDoesNotThrow { binaryType.validate() }
+    }
+
+    //SQLBlobType
+    // Test for the format function
+
+    @Test
+    fun `if a ByteArray is formatted with SQLBlobType, then it should be converted to a full hexadecimal string`() {
+        val blobType = SQLBlobType()
+        val byteArray = byteArrayOf(0x01, 0x02, 0x03, 0x04)
+        val result = blobType.format(byteArray)
+        assertEquals("0x01020304", result)
+    }
+
+    @Test
+    fun `if value is castable to ByteArray in SQLBlobType, then it should be correctly formatted`() {
+        val blobType = SQLBlobType()
+        val byteArray = "Test".toByteArray()
+        val result = blobType.format(byteArray as ByteArray) // Explicit cast in the test
+        assertEquals("0x54657374", result) // ASCII (or UTF-8) values of 'Test'
+    }
+
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+    // Test for the format function
+
+    @Test
+    fun `if a Date is formatted with SQLDateType, then it should be converted to a yyyy-MM-dd string`() {
+        val dateType = SQLDateType()
+        val date = Date(1625155200000L) // Represents 2021-07-01
+        val result = dateType.format(date)
+        assertEquals("'2021-07-01'", result)
+    }
+
+    @Test
+    fun `if a LocalDateTime is formatted with SQLDateType, then it should be converted to a yyyy-MM-dd string`() {
+        val dateType = SQLDateType()
+        val localDateTime = LocalDateTime.of(2021, 7, 1, 12, 0)
+        val date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant())
+        val result = dateType.format(date)
+        assertEquals("'2021-07-01'", result)
+    }
+
+    @Test
+    fun `if a String representing a date is formatted with SQLDateType, then it should be parsed and formatted to yyyy-MM-dd`() {
+        val dateType = SQLDateType()
+        val dateString = "2021-07-01"
+        val date = dateFormat.parse(dateString)
+        val result = dateType.format(date)
+        assertEquals("'2021-07-01'", result)
+    }
+
+    // Test for error scenarios
+
+    @Test
+    fun `if an invalid string is formatted with SQLDateType, then it should throw an exception`() {
+        val dateType = SQLDateType()
+        val invalidDateString = "invalid-date"
+        assertThrows<IllegalArgumentException> {
+            dateType.format(invalidDateString)
+        }
+    }
 }
